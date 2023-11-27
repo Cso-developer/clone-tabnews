40ea373dc777124ed10632ab9cca8c8f4079c282
Cso-developer <cleverton.dev@gmail.com>
2023-11-27 20:52:49 +0000

    "Adiciona scripts `lint: check` e `lint:fix`"

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home(){
-
-    return <h1>Faça teu melhor nas condições que voçê tem, até ter condições de fazer melhor "Cortela".</h1>
+function Home() {
+  return (
+    <h1>
+      Faça teu melhor nas condições que voçê tem, até ter condições de fazer
+      melhor "Cortela".
+    </h1>
+  );
 }
 
 export default Home;
